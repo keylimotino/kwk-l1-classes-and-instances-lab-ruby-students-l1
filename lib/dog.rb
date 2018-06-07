066c2bb917e800ab6cbed827a440db5b82ee3716
keylimotino <stephanie48758@gmail.com>
2018-06-07 19:22:39 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,29 +2,7 @@
 
 class Dog
 
-  def initialize(fido)
-    @fido = fido
-  end
 
-  def fido
-    @fido
-  end
-
-  def snoopy=(snoopy)
-    @snoopy = snoopy
-  end
-
-  def snoopy
-    @snoopy
-  end
-
-  def lassie=(lassie)
-    @lassie = lassie
-  end
-
-  def snoopy
-    @snoopy
-  end
 
 end
 
