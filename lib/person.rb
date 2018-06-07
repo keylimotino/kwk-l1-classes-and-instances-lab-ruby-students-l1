6d1b03be1e8b55b26c21c86a01f5929609ca3c96
keylimotino <stephanie48758@gmail.com>
2018-06-07 19:24:39 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,20 +2,7 @@
 
 class Person
 
-  def adele_goldberg= (adele_goldberg)
-    @adele_goldberg = adele_goldberg
-  end
-
-  def adele_goldberg
-    @adele_goldberg
-  end
-
-  def alan_kay= (alan_kay)
-    @alan_kay = alan_kay
-  end
-
-  def alan_kay
-    @alan_kay
-  end
-
 end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
