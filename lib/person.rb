1e464688dc3c96cc26e7490303a0137da92e63b3
keylimotino <stephanie48758@gmail.com>
2018-06-07 19:17:59 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,21 @@
 # person.rb
+
+class Person
+
+  def adele_goldberg= (adele_goldberg)
+    @adele_goldberg = adele_goldberg
+  end
+
+  def adele_goldberg
+    @adele_goldberg
+  end
+
+  def alan_kay= (alan_kay)
+    @alan_kay = alan_kay
+  end
+
+  def alan_kay
+    @alan_kay
+  end
+
+end
