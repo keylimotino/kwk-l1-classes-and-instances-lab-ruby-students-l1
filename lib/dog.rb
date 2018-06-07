a5f62c526df8e0cbd5dc63c132c2ee8b5c1d0066
keylimotino <stephanie48758@gmail.com>
2018-06-07 19:10:04 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,29 @@
 # dog.rb
 
 class Dog
-  
+
+  def fido(fido)
+    @fido = fido
+  end
+
+  def fido
+    @fido
+  end
+
+  def snoopy(snoopy)
+    @snoopy = snoopy
+  end
+
+  def snoopy
+    @snoopy
+  end
+
+  def lassie(lassie)
+    @lassie = lassie
+  end
+
+  def snoopy
+    @snoopy
+  end
+
+end
